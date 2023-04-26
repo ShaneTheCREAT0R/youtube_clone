fd625e2b2d86f976073716662311f0b5ea788673
Shane Pugmire <shaynstr@gmail.com>
2023-04-26 16:19:56 -0700

    added code and made sidebar file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material';
 import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
@@ -8,7 +7,7 @@ const App = () => (
     <Box sx={{ backgroundColor: '#000'}}>
         <Navbar />
         <Routes>
-            <Route path="/" exact element={<Feed />} />
+            <Route exact path='/' element={<Feed />} />
             <Route path="/video/:id" element={<VideoDetail />} />
             <Route path="/channel/:id" element={<ChannelDetail />} />
             <Route path="/search/:searchTerm" element={<SearchFeed />} />
