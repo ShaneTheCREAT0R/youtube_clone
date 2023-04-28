ff0118f9bcc777ad791e1ee23db04b7ed5c4c026
Shane Pugmire <shaynstr@gmail.com>
2023-04-28 14:14:39 -0700

    added a title and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { logo } from '../utils/constants';
 import SearchBar from './SearchBar';
@@ -8,7 +8,12 @@ import SearchBar from './SearchBar';
 const Navbar = () => (
   <Stack direction='row' alignItems="center" p={2} sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}>
    <Link to="/" style={{ display: 'flex', alignItems: 'center'}}>
-    <img src={logo} alt="logo" height={45} />
+    <img src={logo} alt="logo" height={45} color="fff"/> 
+        <Typography variant="h5" fontWeight="bold" mb={2} sx={{color: 'white'}} paddingTop={2} pl={2} >
+           <span>
+            YouTube Clone App
+          </span>
+        </Typography>
    </Link>
    <SearchBar />
   </Stack>
